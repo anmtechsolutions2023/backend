1192e0502ecbb6fb631ac6a5bbf567ad8a5d5ff8
Animesh <animesh.tradepeeps@gmail.com>
2025-01-06 22:06:34 +0530

    Code Refactor for location detail

--- AccountingSoftware/app/models/locationdetail.model.js
+++ AccountingSoftware/app/models/locationdetail.model.js
@@ -1,201 +1,130 @@
 const { v4: uuidv4 } = require('uuid')
-const sql = require('./db.js')
-const statuses = require('./statuses.js')
 const logger = require('../utils/loggerHelper')
 const moduleNames = require('../config/modulenames')
 const moduleScripts = require('../../Scripts/modelscripts.js')
+const statusCodes = require('../config/statusCodes.js')
+const getAllModels = require('../models/common/getAll.model')
+const getFindById = require('../models/common/findById.model')
+const deleteById = require('../models/common/deleteById.model')
+const handleDatabaseError = require('../common/errorhandle.common')
+const i18n = require('../utils/i18n')
+const mysqlConnection = require('../utils/db.js')
 
-exports.delete = (id, tenantId, username) => {
-  return new Promise((resolve, reject) => {
-    let query = moduleScripts.locationdetail.delete
-
-    sql.query(query, [id, tenantId], (err, res) => {
-      if (err) {
-        logger.loggerHelper(
-          tenantId,
-          username,
-          moduleNames.locationdetail.db.delete,
-          logger.logType.error,
-          `Error for Id: ${id}, Error Code: ${err.code}, Error: ${err}`
-        )
-        return reject('DB Error, for operation:  delete' + err)
-      }
-
-      if (JSON.stringify(res.affectedRows)) {
-        logger.loggerHelper(
-          tenantId,
-          username,
-          moduleNames.locationdetail.db.delete,
-          logger.logType.debug,
-          `Record Deleted, Id ${id}`
-        )
-        resolve(res)
-      } else {
-        logger.loggerHelper(
-          tenantId,
-          username,
-          moduleNames.locationdetail.db.delete,
-          logger.logType.error,
-          `Record not found for Id: ${id}`
-        )
-        resolve(statuses.Statuses.NotFound)
-      }
-    })
-  })
+exports.deleteById = async (id, tenantId, username) => {
+  return await deleteById.deleteById(
+    id,
+    tenantId,
+    username,
+    moduleScripts.locationdetail.delete,
+    moduleNames.locationdetail.db.delete
+  )
 }
 
-exports.getAll = (tenantId, username) => {
-  return new Promise((resolve, reject) => {
-    let query = moduleScripts.locationdetail.fetchAll
-
-    sql.query(query, [tenantId], (err, res) => {
-      if (err) {
-        logger.loggerHelper(
-          tenantId,
-          username,
-          moduleNames.locationdetail.db.fetchAll,
-          logger.logType.error,
-          `Error Code: ${err.code}, Error: ${err}`
-        )
-        return reject('DB Error, for operation:  getAll.' + err)
-      }
-
-      logger.loggerHelper(
-        tenantId,
-        username,
-        moduleNames.locationdetail.db.fetchAll,
-        logger.logType.debug,
-        'Success'
-      )
-      resolve(res)
-    })
-  })
+exports.getAll = async (tenantId, username) => {
+  return await getAllModels.getAll(
+    tenantId,
+    username,
+    moduleScripts.locationdetail.fetchAll,
+    moduleNames.locationdetail.db.fetchAll
+  )
 }
 
-exports.findById = (id, tenantId, username, callerModule) => {
-  return new Promise((resolve, reject) => {
-    let query = moduleScripts.locationdetail.fetchById
-
-    sql.query(query, [id, tenantId], (err, res) => {
-      if (err) {
-        logger.loggerHelper(
-          tenantId,
-          username,
-          `${callerModule}--${moduleNames.locationdetail.db.fetchById}`,
-          logger.logType.error,
-          `Error for Id: ${id}, Error Code: ${err.code}, Error: ${err}`
-        )
-        return reject('DB Error, for operation:  findById' + err)
-      }
-
-      if (res.length) {
-        logger.loggerHelper(
-          tenantId,
-          username,
-          `${callerModule}--${moduleNames.locationdetail.db.fetchById}`,
-          logger.logType.debug,
-          `Record found for Id: ${id}`
-        )
-        resolve(res)
-      } else {
-        logger.loggerHelper(
-          tenantId,
-          username,
-          `${callerModule}--${moduleNames.locationdetail.db.fetchById}`,
-          logger.logType.debug,
-          `Record not found for Id: ${id}`
-        )
-        resolve(statuses.Statuses.NotFound)
-      }
-    })
-  })
+exports.findById = async (id, tenantId, username, callerModule) => {
+  return await getFindById.findById(
+    id,
+    tenantId,
+    username,
+    moduleScripts.locationdetail.fetchById,
+    `${callerModule}--${moduleNames.locationdetail.db.fetchById}`
+  )
 }
 
-exports.update = (locationDetailReq, username) => {
-  return new Promise((resolve, reject) => {
-    let query = moduleScripts.locationdetail.update
+exports.update = async (locationDetailReq, username) => {
+  try {
+    const query = moduleScripts.locationdetail.update
 
-    sql.query(
-      query,
-      [
-        locationDetailReq.Lat,
-        locationDetailReq.Lng,
-        locationDetailReq.CF1,
-        locationDetailReq.CF2,
-        locationDetailReq.CF3,
-        locationDetailReq.CF4,
-        locationDetailReq.Active,
-        locationDetailReq.UpdatedOn,
-        locationDetailReq.UpdatedBy,
-        locationDetailReq.Id,
-        locationDetailReq.TenantId,
-      ],
-      (err, res) => {
-        if (err) {
-          logger.loggerHelper(
-            locationDetailReq.TenantId,
-            username,
-            moduleNames.locationdetail.db.update,
-            logger.logType.error,
-            `Error for Id: ${locationDetailReq.Id}, Error Code: ${err.code}, Error: ${err}`
-          )
-          return reject(err.code)
-        }
+    await mysqlConnection.query(query, [
+      locationDetailReq.Lat,
+      locationDetailReq.Lng,
+      locationDetailReq.CF1,
+      locationDetailReq.CF2,
+      locationDetailReq.CF3,
+      locationDetailReq.CF4,
+      locationDetailReq.Active,
+      locationDetailReq.UpdatedOn,
+      locationDetailReq.UpdatedBy,
+      locationDetailReq.Id,
+      locationDetailReq.TenantId,
+    ])
 
-        logger.loggerHelper(
-          locationDetailReq.TenantId,
-          username,
-          moduleNames.locationdetail.db.update,
-          logger.logType.debug,
-          `Successfully updated Id: ${locationDetailReq.Id}`
-        )
-        resolve(res)
-      }
+    logger.loggerHelper(
+      locationDetailReq.TenantId,
+      username,
+      moduleNames.locationdetail.db.update,
+      logger.logType.debug,
+      i18n.__('messages.logger.successUpdatedById', {
+        id: locationDetailReq.Id,
+      })
     )
-  })
+
+    return statusCodes.HTTP_STATUS_OK
+  } catch (err) {
+    logger.loggerHelper(
+      locationDetailReq.TenantId,
+      username,
+      moduleNames.locationdetail.db.update,
+      logger.logType.error,
+      i18n.__('messages.logger.errorUpdatedById', {
+        id: locationDetailReq.Id,
+        code: err.code,
+        message: err,
+      })
+    )
+
+    throw handleDatabaseError.handleDatabaseError(err)
+  }
 }
 
-exports.create = (locationDetailReq, username) => {
-  return new Promise((resolve, reject) => {
+exports.create = async (locationDetailReq, username) => {
+  try {
     let query = moduleScripts.locationdetail.create
     let locationDetailId = uuidv4()
 
-    sql.query(
-      query,
-      [
-        locationDetailId,
-        locationDetailReq.Lat,
-        locationDetailReq.Lng,
-        locationDetailReq.CF1,
-        locationDetailReq.CF2,
-        locationDetailReq.CF3,
-        locationDetailReq.CF4,
-        locationDetailReq.Active,
-        locationDetailReq.TenantId,
-        locationDetailReq.CreatedOn,
-        locationDetailReq.CreatedBy,
-      ],
-      (err, res) => {
-        if (err) {
-          logger.loggerHelper(
-            locationDetailReq.TenantId,
-            username,
-            moduleNames.locationdetail.db.create,
-            logger.logType.error,
-            `Error for creating record: ${locationDetailReq.Lat}-${locationDetailReq.Lng}, Error Code: ${err.code} Error: ${err}`
-          )
-          return reject(err.code)
-        }
+    await mysqlConnection.query(query, [
+      locationDetailId,
+      locationDetailReq.Lat,
+      locationDetailReq.Lng,
+      locationDetailReq.CF1,
+      locationDetailReq.CF2,
+      locationDetailReq.CF3,
+      locationDetailReq.CF4,
+      locationDetailReq.Active,
+      locationDetailReq.TenantId,
+      locationDetailReq.CreatedOn,
+      locationDetailReq.CreatedBy,
+    ])
 
-        logger.loggerHelper(
-          locationDetailReq.TenantId,
-          username,
-          moduleNames.locationdetail.db.create,
-          logger.logType.debug,
-          `Successfully created with Id: ${locationDetailId}`
-        )
-        resolve(locationDetailId)
-      }
+    logger.loggerHelper(
+      locationDetailReq.TenantId,
+      username,
+      moduleNames.locationdetail.db.create,
+      logger.logType.debug,
+      i18n.__('messages.logger.successCreatedById', { id: locationDetailId })
     )
-  })
+
+    return locationDetailId
+  } catch (err) {
+    logger.loggerHelper(
+      locationDetailReq.TenantId,
+      username,
+      moduleNames.locationdetail.db.create,
+      logger.logType.error,
+      i18n.__('messages.logger.errorCreatedById', {
+        name: `${locationDetailReq.Lat}-${locationDetailReq.Lng}`,
+        code: err.code,
+        message: err,
+      })
+    )
+    throw handleDatabaseError.handleDatabaseError(err)
+  }
 }
